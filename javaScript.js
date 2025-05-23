55f849618385c40e5fe7664f5642b76a93a60052
Md_Nayem <lnrs.nayem@gmail.com>
2025-05-23 06:39:30 +0600

    operators practice

--- javaScript.js
+++ javaScript.js
@@ -1,2 +1,42 @@
-console.log("hello");
-console.log("object");
+// JavaScripts operators
+
+/*
+1. Arithmetic Operators
+2. Assignment Operators
+3. Comparison Operators
+4. Logical Operators
+5. Bitwise Operators
+*/
+
+
+// Arithmetic Operators
+let a = 30;
+let b = 15;
+// console.log(a + b); // addition
+
+let c = a + b;
+console.log(c);
+console.log(a - b); // subtraction
+console.log(a * b); // Multiplication
+console.log(a/b); // division
+console.log(a % b ); // modulus
+
+/*
+// real life example to modulus operators
+let userInput = prompt("Enter a Number");
+let number = parseInt(userInput);
+
+if (number  % 2 == 0) {
+    console.log(`${number} is even number`);
+}else {
+    console.log(`${number} is odd number`);
+}
+*/
+
+// increment and decrement operators
+let x = 10;
+x = x + 1; // increment
+y = x - 1; // decrement
+console.log(x);
+console.log(y);
+
