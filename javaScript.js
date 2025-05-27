e5b54541a628ded61af1312b1a225eacc97017f9
Md_Nayem <lnrs.nayem@gmail.com>
2025-05-28 00:36:23 +0600

    operators Practice for learning

--- javaScript.js
+++ javaScript.js
@@ -6,6 +6,7 @@
 3. Comparison Operators
 4. Logical Operators
 5. Bitwise Operators
+6. Ternary (conditional) Operator
 */
 
 
@@ -15,7 +16,7 @@ let b = 15;
 // console.log(a + b); // addition
 
 let c = a + b;
-console.log(c);
+console.log(c); // addition
 console.log(a - b); // subtraction
 console.log(a * b); // Multiplication
 console.log(a/b); // division
@@ -39,4 +40,62 @@ x = x + 1; // increment
 y = x - 1; // decrement
 console.log(x);
 console.log(y);
+
+// Assignment Operators
+let d = 5;
+let e = 8;
+d += e; // d = d + e
+console.log(d); // 13
+d -= e; // d = d - e
+console.log(d); // 5
+d *= e; // d = d * e
+console.log(d); // 40
+d /= e; // d = d / e
+console.log(d); // 5
+d %= e; // d = d % e
+console.log(d); // 5
+d **= e; // d = d ** e
+console.log(d); // 390625 
+
+
+// comparison operators
+let num1 = 10;
+let num2 = 15;
+console.log(num1 == num2); // equal to
+console.log(num1 === num2); // strict equal to
+console.log(num1 != num2); // not equal to
+console.log(num1 !== num2);  // strict not equal to
+console.log(num1 > num2); // greater than
+console.log(num1 < num2); // less than
+console.log(num1 >= num2); // greater than or equal to
+console.log(num1 <= num2);  // less than or equal to
+
+
+// logical operators
+let p = true;
+let q = false;
+console.log(p && q); // logical AND
+console.log(p || q); // logical OR
+console.log(!p); // logical NOT
+
+// Bitwise Operators
+let m = 1; // 0001 in binary
+let n = 2; // 0010 in binary
+console.log(m & n); 
+
+// Bitwise AND
+console.log(m | n); // Bitwise OR
+console.log(m ^ n); // Bitwise XOR
+console.log(~m); // Bitwise NOT
+console.log(m << 1); // Bitwise left shift
+console.log(m >> 1); // Bitwise right shift
+
+// Ternary (conditional) Operator
+let age = prompt("Enter your age:");
+age = parseInt(age);;
+if (age >= 18){
+    console.log("You are eligible to vote.");
+}else{
+    console.log("You are not eligible to vote.");
+}
 
