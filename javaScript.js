e0d701b7195dd740c81f1296f8f2a30797ab22f3
Md_Nayem <lnrs.nayem@gmail.com>
2025-05-28 02:29:12 +0600

    dataType practice for learning

--- javaScript.js
+++ javaScript.js
@@ -1,101 +1,103 @@
-// JavaScripts operators
-
+// Data types in javaScript
 /*
-1. Arithmetic Operators
-2. Assignment Operators
-3. Comparison Operators
-4. Logical Operators
-5. Bitwise Operators
-6. Ternary (conditional) Operator
-*/
+1. String
+2. Number
+3. Boolean
+4. Undefined
+5. Null
+6. array
+7. BigInt
+8. Object
+*/  
 
 
-// Arithmetic Operators
-let a = 30;
-let b = 15;
-// console.log(a + b); // addition
+// string
+let name = "md nayem";
+console.log(typeof name); // string
 
-let c = a + b;
-console.log(c); // addition
-console.log(a - b); // subtraction
-console.log(a * b); // Multiplication
-console.log(a/b); // division
-console.log(a % b ); // modulus
-
-/*
-// real life example to modulus operators
-let userInput = prompt("Enter a Number");
-let number = parseInt(userInput);
-
-if (number  % 2 == 0) {
-    console.log(`${number} is even number`);
-}else {
-    console.log(`${number} is odd number`);
-}
-*/
-
-// increment and decrement operators
-let x = 10;
-x = x + 1; // increment
-y = x - 1; // decrement
-console.log(x);
-console.log(y);
-
-// Assignment Operators
-let d = 5;
-let e = 8;
-d += e; // d = d + e
-console.log(d); // 13
-d -= e; // d = d - e
-console.log(d); // 5
-d *= e; // d = d * e
-console.log(d); // 40
-d /= e; // d = d / e
-console.log(d); // 5
-d %= e; // d = d % e
-console.log(d); // 5
-d **= e; // d = d ** e
-console.log(d); // 390625 
+let firstName = "md";
+let medelName = "nayem";
+let lastName = "Islam";
+fullName = `my name is ${firstName} ${medelName}`;
+console.log(fullName); // my name is md nayem
+fullName = firstName +" " + medelName + " " + lastName;
+fullName = fullName.toUpperCase(); // MD NAYEM ISLAM
+console.log(fullName); // MD NAYEM ISLAM
 
 
-// comparison operators
-let num1 = 10;
-let num2 = 15;
-console.log(num1 == num2); // equal to
-console.log(num1 === num2); // strict equal to
-console.log(num1 != num2); // not equal to
-console.log(num1 !== num2);  // strict not equal to
-console.log(num1 > num2); // greater than
-console.log(num1 < num2); // less than
-console.log(num1 >= num2); // greater than or equal to
-console.log(num1 <= num2);  // less than or equal to
+// number
+let age = 25;
+console.log(typeof age); // number
+let number1 = 10;
+let number2 = 20;
+let sum = "My name is "+fullName+ ", I am "+age+" years Old "; // 30
+
+console.log(sum); // 30
+console.log(number1 + number2); // 30
+
+let pi = 3.14;
+console.log(typeof pi); // number
+
+let numberBig = 12345678901234567890n // BigInt
+let numberBig2 = BigInt(12345678901234567890); // BigInt
+console.log(typeof numberBig); // bigint
+console.log(typeof numberBig2); // bigint
 
 
-// logical operators
-let p = true;
-let q = false;
-console.log(p && q); // logical AND
-console.log(p || q); // logical OR
-console.log(!p); // logical NOT
+// boolean
+let isMarried = false;
+console.log(typeof isMarried); // boolean
+let isSingle = true;
+console.log(typeof isSingle); // boolean
 
-// Bitwise Operators
-let m = 1; // 0001 in binary
-let n = 2; // 0010 in binary
-console.log(m & n); 
 
-// Bitwise AND
-console.log(m | n); // Bitwise OR
-console.log(m ^ n); // Bitwise XOR
-console.log(~m); // Bitwise NOT
-console.log(m << 1); // Bitwise left shift
-console.log(m >> 1); // Bitwise right shift
+// undefined
+let someInfo;
+console.log(typeof someInfo); // undefined
+someInfo = "I am a web developer";
+console.log(someInfo); // I am a web developer
 
-// Ternary (conditional) Operator
-let age = prompt("Enter your age:");
-age = parseInt(age);;
-if (age >= 18){
-    console.log("You are eligible to vote.");
-}else{
-    console.log("You are not eligible to vote.");
-}
+// null
+let nullValue = null; 
+console.log(typeof nullValue); // object
 
+
+// array
+let numbers = [1, 2, 3, 4, 5, 2, 1, 9, 5, 7, 8, 3, 6, 4]
+console.log(typeof numbers); // array
+console.log(numbers.length); // 14
+numbers[0] = 0; // changing the first element
+numbers[1] = 1; // changing the second element
+console.log(numbers[0]); // 1
+console.log(numbers[1]); // 2
+console.log(numbers[2]); // 3
+console.log(numbers[3]); // 4
+console.log(numbers[4]); // 5
+console.log(numbers[5]); // 2
+console.log(numbers[6]); // 1
+console.log(numbers[7]); // 9
+console.log(numbers[8]); // 5
+console.log(numbers[9]); // 7
+console.log(numbers[10]); // 8
+console.log(numbers[11]); // 3
+console.log(numbers[12]); // 6
+console.log(numbers[13]); // 4
+console.log(numbers); // [1, 2, 3, 4, 5, 2, 1, 9, 5, 7, 8, 3, 6, 4]
+
+
+// object
+let person = {
+    name: "md nayem",
+    age: 25,
+    isMarried: false,
+    address: {
+        city: "Dhaka",
+        country: "Bangladesh"},
+    number : "01712345678",
+};
+console.log(typeof person); // object
+console.log(person.name); // md nayem
+console.log(person.age); // 25
+console.log(person.address.city); // Dhaka
+console.log(person.address.counrty); // Bangladesh);
+console.log(person.number); // 01712345678
